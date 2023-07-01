fb9177bf475deb26034a09913e1ef40da9dcc57f
Abhishek <you@example.com>
2023-07-01 16:00:23 +0530

    1-07-2023 update2

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const userRoute = require("./routes/user");
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+mongoose.connect( "mongodb+srv://DHRUVA:DHRUVA@cluster0.lp8ku9c.mongodb.net/LOVETODO?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('MongoDB connected'))
+	.catch((err) => console.log('Error connecting to MongoDB', err));
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(bodyParser.json())
+app.use('/public',express.static('public'))
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+app.use("/api/v1/users", userRoute);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listenting on port ${port}...`));
